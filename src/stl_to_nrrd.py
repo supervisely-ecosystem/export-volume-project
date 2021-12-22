b34fe599f1ae343a475918bee83e5dc53b23f155
cxnt <pavelbartsits@gmail.com>
2021-12-22 13:27:30 +0300

    save stl and nrrd file to interpolation folder

--- src/stl_to_nrrd.py
+++ src/stl_to_nrrd.py
@@ -54,6 +54,10 @@ def vec3_mat4(vec, mat):
     return (v / v[3])[:-1]
 
 
+def clamp_val(val, min_val, max_val):
+    return max(min(val, min_val), max_val)
+
+
 def convert_stl_to_nrrd(nrrd_path, stl_path, output_file_path):
     nrrd_header = nrrd.read_header(nrrd_path)
 
@@ -65,7 +69,7 @@ def convert_stl_to_nrrd(nrrd_path, stl_path, output_file_path):
     min_vec = [float('inf'), float('inf'), float('inf')]
     max_vec = [float('-inf'), float('-inf'), float('-inf')]
 
-    mesh.apply_scale((-1, -1, 1)) # LPS to RAS
+    mesh.apply_scale((-1, -1, 1))  # LPS to RAS
     mesh.apply_transform(world_to_voxel)
 
     for vert in mesh.vertices:
@@ -82,19 +86,32 @@ def convert_stl_to_nrrd(nrrd_path, stl_path, output_file_path):
     voxel = mesh.voxelized(pitch=1.0)
     voxel = voxel.fill()
 
-    vol_shape = nrrd_header['sizes']
     mask = voxel.matrix.astype(np.short)
+
+    vol_shape = nrrd_header['sizes']
     padded_mask = np.zeros(vol_shape).astype(np.short)
 
-    offsets = [math.ceil(center_v - shape_v / 2) for center_v, shape_v in zip(center, mask.shape)]
+    # find dimension coords
+    start = [math.ceil(center_v - shape_v / 2) for center_v, shape_v in zip(center, mask.shape)]
+    end = [math.ceil(center_v + shape_v / 2) for center_v, shape_v in zip(center, mask.shape)]
+
+    # find intersections
+    vol_inter_max = [max(start[0], 0), max(start[1], 0), max(start[2], 0)]
+    vol_inter_min = [min(end[0], vol_shape[0]), min(end[1], vol_shape[1]), min(end[2], vol_shape[2])]
 
     padded_mask[
-       offsets[0]:mask.shape[0] + offsets[0],
-       offsets[1]:mask.shape[1] + offsets[1],
-       offsets[2]:mask.shape[2] + offsets[2],
-    ] = mask
+    vol_inter_max[0]:vol_inter_min[0],
+    vol_inter_max[1]:vol_inter_min[1],
+    vol_inter_max[2]:vol_inter_min[2],
+    ] = mask[
+        vol_inter_max[0] - start[0]:vol_inter_min[0] - start[0],
+        vol_inter_max[1] - start[1]:vol_inter_min[1] - start[1],
+        vol_inter_max[2] - start[2]:vol_inter_min[2] - start[2],
+        ]
 
     mask = padded_mask
+
+    # print('nrrd_header', nrrd_header)
 
     nrrd.write(
         output_file_path,

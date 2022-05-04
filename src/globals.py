4a58a3448b043d6d0bb5a1b012922c0171e66ef8
cxnt <pavelbartsits@gmail.com>
2022-05-04 16:45:49 +0300

    comment debug lines

--- src/globals.py
+++ src/globals.py
@@ -4,7 +4,7 @@ from distutils.util import strtobool
 from pathlib import Path
 
 import supervisely as sly
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from supervisely.app.v1.app_service import AppService
 
 root_source_dir = str(Path(sys.argv[0]).parents[1])
@@ -12,10 +12,10 @@ print(f"App source directory: {root_source_dir}")
 sys.path.append(root_source_dir)
 
 # only for convenient debug
-debug_env_path = os.path.join(root_source_dir, "debug.env")
-secret_debug_env_path = os.path.join(root_source_dir, "secret_debug.env")
-load_dotenv(debug_env_path)
-load_dotenv(secret_debug_env_path, override=True)
+# debug_env_path = os.path.join(root_source_dir, "debug.env")
+# secret_debug_env_path = os.path.join(root_source_dir, "secret_debug.env")
+# load_dotenv(debug_env_path)
+# load_dotenv(secret_debug_env_path, override=True)
 
 
 api: sly.Api = sly.Api.from_env()
